16bb1164d07a69aaefa36a247d8d962d46dc6d67
Andrea Ng <157224183+whoaducks26@users.noreply.github.com>
2024-10-07 12:28:10 +0800

    Update TeamQomo_Flag10.py

--- TeamQomo_Flag10.py
+++ TeamQomo_Flag10.py
@@ -1,1 +1,24 @@
+# flag 10
 
+artifacts = [(1, 10, 2),
+             (2, 20, 3),
+             (3, 15, 1),
+             (4, 30, 4),
+             (5, 25, 2)]
+
+max_weight = 6
+
+possible_id_combos = [(1, 2, 3), (2, 3, 5), (1, 4), (4, 5)]
+
+highest = 0
+for x in possible_id_combos:
+    temp = 0
+    for id_num in x:
+        points = artifacts[id_num-1][1]
+        temp += points
+    if temp > highest:
+        highest = temp
+        ids = x
+
+print(highest)
+print(ids)
