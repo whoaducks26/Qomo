cb7caf6da8c1ce25c57b5b3e64879efbeb8eaa75
Andrea Ng <157224183+whoaducks26@users.noreply.github.com>
2024-10-07 11:22:19 +0800

    Update TeamQomo_Flag04.py

--- TeamQomo_Flag04.py
+++ TeamQomo_Flag04.py
@@ -1,1 +1,24 @@
-o
+# Flag 4
+
+riddle_num = 10
+
+alpha = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+arr = alpha.split(" ")
+# print(arr)
+message = "S vyfo Robwsyxo Qbkxqob!"
+new_message = ""
+
+for char in message:
+    if char.isalpha():
+        if char.islower():
+            temp_num = arr.index(char)
+            ori_num = temp_num-10
+            new_message += arr[ori_num]
+        else:
+            temp_num = ord(char)
+            ori_num = temp_num-10
+            new_message += chr(ori_num)
+    else:
+        new_message += char
+
+print(new_message)
