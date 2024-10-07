edbc6a3a49a3da0f943c9b33f49806b2cdfdc934
Robbin1Yrew <blaziken994@gmail.com>
2024-10-07 11:41:26 +0800

    Update TeamQomo_Flag03.py

--- TeamQomo_Flag03.py
+++ TeamQomo_Flag03.py
@@ -1,1 +1,25 @@
-t
+spellbook1 = {
+    "Elemental Magic": ["Fireball", "Lightning Bolt", "Earthquake"],
+    "Healing Magic": ["Heal", "Greater Heal"],
+    "Dark Magic": ["Curse"]
+}
+
+spellbook2 = {
+    "Elemental Magic": ["Fireball", "Ice Storm"],
+    "Healing Magic": ["Heal"],
+    "Necromancy": ["Raise Undead", "Life Drain"]
+}
+
+#spellbook3 (merge both spellbooks together)
+merged_spellbook = spellbook1.copy()
+
+for i in spellbook2:
+
+    for j in spellbook2[i]:
+        if i not in merged_spellbook:
+            merged_spellbook[i] = [j]
+        if j not in merged_spellbook[i]:
+            merged_spellbook[i].append(j)
+
+print(merged_spellbook)
+
