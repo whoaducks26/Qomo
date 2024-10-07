e3b73dcafc047c61147aff7810e48af7951ee618
fingleBerryMango <clydekoh777@gmail.com>
2024-10-07 13:30:00 +0800

    Update TeamQomo_Flag13.py

--- TeamQomo_Flag13.py
+++ TeamQomo_Flag13.py
@@ -1,1 +1,14 @@
-hi
+p = 53
+q = 97
+message = "!L0VEH@RRYP0TTER"
+e = 65537
+
+n = p * q
+encrypted_values = []
+for i in message:
+    c = ((ord(i) + 1000) ** e) % n
+    c = ((c + 1000) ** e) % n
+    c = ((c + 1000) ** e) % n
+    encrypted_values.append(c)
+
+print(encrypted_values)
