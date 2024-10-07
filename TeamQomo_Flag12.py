ac04d972db7537fb3f3237c13db9632c8bd2bcc4
Robbin1Yrew <blaziken994@gmail.com>
2024-10-07 12:31:02 +0800

    Update TeamQomo_Flag12.py

--- TeamQomo_Flag12.py
+++ TeamQomo_Flag12.py
@@ -1,1 +1,23 @@
+rune_list = [
+    ['trick', 20, 30], 10, 'cursed', 40, 'broken', 20, 'none', 30,
+    ]
+
+target_value = 0
+
+for i in range(len(rune_list)):
+    try:
+        if type(rune_list[i]) == list:
+            for j in range(len(rune_list[i])):
+                try:
+                    target_value += rune_list[i][j]
+                except TypeError:
+                    continue
+
+        target_value += rune_list[i]
+
+    except TypeError:
+        continue
+    
+
+print(target_value)
 
