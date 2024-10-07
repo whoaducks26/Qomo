c6b604623f2feeee7dbc02c9548944c97b243a3b
fingleBerryMango <clydekoh777@gmail.com>
2024-10-07 11:07:26 +0800

    Update TeamQomo_Flag02.py

--- TeamQomo_Flag02.py
+++ TeamQomo_Flag02.py
@@ -1,1 +1,13 @@
-o
+seats = eval(input("Input seats: "))
+enchanted_rows = eval(input("Input enchanted rows: "))
+
+def can_see_stage(seats, enchanted_rows):
+    for i in enchanted_rows:
+        for j in range(len(seats[i])):
+            if seats[i][j] - seats[i-1][j] < 2:
+                return False
+            else:
+                continue
+    return True
+
+can_see_stage(seats,enchanted_rows)
