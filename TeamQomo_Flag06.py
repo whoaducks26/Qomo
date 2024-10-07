a7b75ebc01a2af61f2af18add98887e92501957c
Andrea Ng <157224183+whoaducks26@users.noreply.github.com>
2024-10-07 11:05:06 +0800

    Update TeamQomo_Flag06.py

--- TeamQomo_Flag06.py
+++ TeamQomo_Flag06.py
@@ -1,1 +1,21 @@
-g
+# Flag 6
+
+# fixed code
+def sort_crystals_by_power(crystal_powers):
+    positive_powers = [p for p in crystal_powers if int(p) >= 0]
+    # print(positive_powers)
+    negative_powers = [p for p in crystal_powers if int(p) < 0]
+    # print(negative_powers)
+    # sorted_powers = positive_powers.sort()
+    positive_powers.sort(reverse=True)
+    # print(positive_powers)
+    final_powers = positive_powers + negative_powers
+    print(final_powers)
+    return f"GIT{final_powers[0]}_{final_powers[-1]}"
+
+crystal_powers = ["15", "42", "-9", "33", "57", "-23"]
+sorted_crystals = sort_crystals_by_power(crystal_powers)
+print(sorted_crystals)
+
+
+#someone help me make the output GIT{57_-23} instead of GIT57_-23
